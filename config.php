29b2816b675803424a9a25af3bf9e87f7492809d
Melvin C V <melrockz@gmail.com>
2022-04-09 12:56:02 +0530

    modified config.php (hidden password)

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_SERVER', 'dev-mysql-db.xxxxxtrxxxx.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'ec2user');
+define('DB_PASSWORD', 'xxxxxxxxxxxxxxx');
+define('DB_NAME', 'crudapi');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
